df41ebeb7f4b5914ce87ec90359ae343d1ff3de7
Lesia Soloviova <solovyova.lesya@gmail.com>
2024-02-03 22:23:00 +0200

    feat: add different ways of styling in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { Fragment } from "react";
-import GenerateProductList from "./components/GenerateProductList";
-import GenerateProductList2 from "./components/GenerateProductList2";
+// import GenerateProductList2 from "./components/GenerateProductList2";
 import data from "./data.json";
-import data2 from "./data.json";
+// import data2 from "./data.json";
+import GenerateProductList from "./components/GenerateProductList/GenerateProductList";
+import styles from "./App.module.css";
 
 const App = () => {
   return (
-    <Fragment>
+    <div className={styles.container}>
       <GenerateProductList data={data} a="qwe" name="qwe" number={123}>
         <div>це children для GenerateProductList</div>
       </GenerateProductList>
       {/* GenerateProductsList({data:data,a:'qwe',name:'qwe',number:123}) */}
-      <GenerateProductList2 data={data2} />
-    </Fragment>
+      {/* <GenerateProductList2 data={data2} /> */}
+    </div>
   );
 };
 
